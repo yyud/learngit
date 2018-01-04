a38399d28c5a9ed647b3a87e78567ac7c28560e6
yyud <990715561@qq.com>
2018-01-04 09:10:25 +0800

    homework

---
+++ Test_03.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test_03 {
+	public static void main(String[] args){
+		System.out.println("    *");
+		System.out.println("   ***");
+		System.out.println("  *****");
+		System.out.println(" *******");
+	}
+}
---
+++ Test_04.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test_04 {
+	public static void main(String[] args){
+		System.out.println("~~~~~~~~~");
+		System.out.println("{/@   @/}");
+		System.out.println("{((00)) }");
+		System.out.println("---------");
+	}
+}
